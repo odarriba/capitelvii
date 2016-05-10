a9bd3b6ff35234094b69735da59ab15483406980
Óscar de Arriba <oscar.dearriba@the-cocktail.com>
2016-05-10 22:59:26 +0200

    Use Devise in the admin namespace.

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -1,5 +1,11 @@
 class Admin::BaseController < ApplicationController
   before_action :authenticate_user!
-  
+
   layout "admin"
+
+  def index
+    respond_to do |format|
+      format.html
+    end
+  end
 end
