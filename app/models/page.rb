288e6e993f1851a3ccc42c85be2cd27e51d40f04
Óscar de Arriba <oscar.dearriba@the-cocktail.com>
2016-05-13 16:59:53 +0200

    Add some model scopes and validations.

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,2 +1,9 @@
 class Page < ActiveRecord::Base
+  validate :title, :slug, presence: true
+
+  default_scope order('order ASC name DESC')
+
+  scope :published, ->() {
+    where(draft: false)
+  }
 end
