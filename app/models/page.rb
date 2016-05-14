392a67e32d49b3f5b8bdf290108235e4cd1fd638
Óscar de Arriba <oscar.dearriba@the-cocktail.com>
2016-05-14 16:42:58 +0200

    Add slug generation to pages model.

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,4 +1,7 @@
 class Page < ActiveRecord::Base
+  extend FriendlyId
+  friendly_id :title, use: [:slugged, :finders]
+
   validates :title, :slug, presence: true
 
   default_scope ->() { order('pages.order ASC, title DESC') }
@@ -6,4 +9,8 @@ class Page < ActiveRecord::Base
   scope :published, ->() {
     where(draft: false)
   }
+
+  def should_generate_new_friendly_id?
+    slug.blank?
+  end
 end
