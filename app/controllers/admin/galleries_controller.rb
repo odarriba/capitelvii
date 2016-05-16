c49fed16f7cb171c81ccb30d89be67b4058e4348
Óscar de Arriba <oscar.dearriba@the-cocktail.com>
2016-05-16 22:57:07 +0200

    Add edit, update and destroy actions, views and locales.

--- app/controllers/admin/galleries_controller.rb
+++ app/controllers/admin/galleries_controller.rb
@@ -1,4 +1,5 @@
 class Admin::GalleriesController < Admin::BaseController
+  before_action :load_gallery, only: [:edit, :update, :destroy]
   def index
     page = params[:page].to_i unless (params[:page].blank? || params[:page].to_i < 1)
     page ||= 1
@@ -25,14 +26,75 @@ class Admin::GalleriesController < Admin::BaseController
   end
 
   def create
+    @gallery = Gallery.new(gallery_params)
+
+    respond_to do |format|
+      format.html {
+        if (@gallery.save)
+          flash[:notice] = t(".success", title: @gallery.title)
+          redirect_to action: :index
+        else
+          flash[:error] = t(".error")
+          render action: :new
+        end
+      }
+    end
   end
 
   def edit
+    return if @gallery.blank?
+
+    respond_to do |format|
+      format.html
+    end
   end
 
   def update
+    return if @gallery.blank?
+
+    respond_to do |format|
+      format.html {
+        if (@gallery.update_attributes(gallery_params))
+          flash[:notice] = t(".success", title: @gallery.title)
+          redirect_to action: :index
+        else
+          flash[:error] = t(".error")
+          render action: :edit
+        end
+      }
+    end
   end
 
   def destroy
+    return if @gallery.blank?
+
+    if (@gallery.destroy)
+      flash[:notice] = t(".success", title: @gallery.title)
+    else
+      flash[:error] = t(".error", title: @gallery.title)
+    end
+
+    respond_to do |format|
+      format.html { redirect_to action: :index }
+    end
+  end
+
+  protected
+
+  # Function to load existing gallery based on ID received in params.
+  #
+  def load_gallery
+    @gallery = Gallery.where(id: params[:id]).first
+
+    if (@gallery.blank?)
+      flash[:error] = t("admin.galleries.not_found")
+      redirect_to action: :index
+    end
+  end
+
+  # Strong Parameters for Gallery objects.
+  #
+  def gallery_params
+    params.require(:gallery).permit(:title, :description, :slug, :position, :draft, :seo_title, :seo_description, :seo_tags)
   end
 end
