4ab3ac4e7a0ffd0997fcc00827df58f8f733b3ac
Óscar de Arriba <oscar.dearriba@the-cocktail.com>
2016-05-16 18:30:30 +0200

    Add default order for Contact model objects.

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,10 +1,12 @@
 class Contact < ActiveRecord::Base
   validates_presence_of :name, :email, :phone, :body
-  
+
   validates :name, length: { in: 2..255 }
   validates :email, length: { in: 6..255 }
   validates :phone, length: { is: 9 }
   validates :body, length: { in: 20..255 }
 
   validates :phone, numericality: { only_integer: true, greater_than: 0 }
+
+  default_scope ->() { order('updated_at DESC') }
 end
