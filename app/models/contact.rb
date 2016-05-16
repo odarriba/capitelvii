94cda5ce49aedefd1ee387df9001c2bb2e4f098d
Óscar de Arriba <oscar.dearriba@the-cocktail.com>
2016-05-16 16:53:49 +0200

    Add Contact model validations.

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,2 +1,9 @@
 class Contact < ActiveRecord::Base
+  validates_presence_of :name, :email, :phone, :body
+  validates :name, length: { in: 2..255 }
+  validates :email, length: { in: 6..255 }
+  validates :phone, length: { is: 9 }
+  validates :body, length: { in: 20..255 }
+
+  validates :phone, numericality: { only_integer: true, greater_than: 0 }
 end
