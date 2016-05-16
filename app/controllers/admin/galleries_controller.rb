5ccfc48852adedeca0d164d7c6c97727aa366052
Óscar de Arriba <oscar.dearriba@the-cocktail.com>
2016-05-16 20:26:08 +0200

    Add index code and view.

--- app/controllers/admin/galleries_controller.rb
+++ app/controllers/admin/galleries_controller.rb
@@ -1,5 +1,19 @@
 class Admin::GalleriesController < Admin::BaseController
   def index
+    page = params[:page].to_i unless (params[:page].blank? || params[:page].to_i < 1)
+    page ||= 1
+
+    max_page = (Page.count*1.0/Page.default_per_page).ceil
+    max_page = 1 if (max_page == 0)
+
+    page = max_page if (page > max_page)
+
+    # Paginate results
+    @galleries = Page.page(page)
+
+    respond_to do |format|
+      format.html
+    end
   end
 
   def new
