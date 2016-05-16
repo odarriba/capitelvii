a54ca3b2d1b91dd7536128e6bd4cdddbc9369319
Óscar de Arriba <oscar.dearriba@the-cocktail.com>
2016-05-16 11:01:15 +0200

    Add subtitle migration and page header.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160514143351) do
+ActiveRecord::Schema.define(version: 20160516085712) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -54,6 +54,7 @@ ActiveRecord::Schema.define(version: 20160514143351) do
     t.boolean  "draft",        default: true
     t.datetime "created_at",                  null: false
     t.datetime "updated_at",                  null: false
+    t.string   "subtitle"
   end
 
   add_index "pages", ["slug"], name: "index_pages_on_slug", unique: true, using: :btree
