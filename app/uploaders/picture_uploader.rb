2627be047f8102e161441a8b6faa94d004393e97
Óscar de Arriba <oscar.dearriba@the-cocktail.com>
2016-05-18 00:02:51 +0200

    Add Lightbox album, css and changes in image uploader.

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -35,7 +35,7 @@ class PictureUploader < CarrierWave::Uploader::Base
 
   # Create different versions of your uploaded files:
   version :thumb do
-    process :resize_to_fill => [100, 100]
+    process :resize_to_fill => [200, 200]
   end
 
   version :medium do
