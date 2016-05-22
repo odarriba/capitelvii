f485f2cf995bdca6f6788592af2b17fd1d792ef7
Óscar de Arriba <oscar.dearriba@the-cocktail.com>
2016-05-22 12:47:31 +0200

    Add route default host, sitemap controller and routes.

--- app/controllers/sitemaps_controller.rb
+++ app/controllers/sitemaps_controller.rb
@@ -1,2 +1,17 @@
 class SitemapsController < ApplicationController
+  def generate
+    builder = Nokogiri::XML::Builder.new do |xml|
+      xml.urlset(xmlns: "http://www.sitemaps.org/schemas/sitemap/0.9") {
+        Page.published.each do |page|
+          xml.url {
+            xml.loc page.url
+            xml.lastmod page.updated_at 
+            xml.priority (page.slug == "/") ? 1.0 : 0.8 
+          }
+        end
+      }
+    end
+
+    render :xml => builder.to_xml
+  end
 end
