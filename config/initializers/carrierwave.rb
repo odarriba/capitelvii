88a0fd6f2f15e9b8a2d56083dd20a61e015d73e5
Óscar de Arriba <oscar.dearriba@the-cocktail.com>
2016-05-22 14:16:52 +0200

    Change fog host to avoid inter-region data charges.

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -3,8 +3,8 @@ CarrierWave.configure do |config|
     provider:              'AWS',
     aws_access_key_id:     Rails.application.secrets.aws['access_key_id'],
     aws_secret_access_key: Rails.application.secrets.aws['secret_access_key'],
+    host:                  "http://s3-#{Rails.application.secrets.aws['region']}.amazonaws.com/",
     region:                Rails.application.secrets.aws['region']
   }
-  config.fog_host = "http://s3-#{Rails.application.secrets.aws['region']}.amazonaws.com/"
   config.fog_directory  = Rails.application.secrets.aws['bucket']
 end
